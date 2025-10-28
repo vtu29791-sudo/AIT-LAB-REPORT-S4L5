fc8c84277ad2935edabaa8e1ccc8838b04615941
vtu29791-sudo <vtu29791@veltech.edu.in>
2025-10-28 11:36:42 +0530

    Add files via upload

---
+++ nqueens.py
@@ -0,0 +1,71 @@
+# Python3 program to solve N Queen problem
+
+N = 4
+
+def printSolution(board):
+    for i in range(N):
+        for j in range(N):
+            if board[i][j] == 1:
+                print("Q", end=" ")
+            else:
+                print(".", end=" ")
+        print()
+
+def isSafe(board, row, col):
+    # Check this row on left side
+    for i in range(col):
+        if board[row][i] == 1:
+            return False
+
+    # Check upper diagonal on left side
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+
+    # Check lower diagonal on left side
+    for i, j in zip(range(row, N, 1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+
+    return True
+
+def solveNQUtil(board, col):
+    # Base case: If all queens are placed, return True
+    if col >= N:
+        return True
+
+    # Consider this column and try placing this queen in all rows one by one
+    for i in range(N):
+        if isSafe(board, i, col):
+            # Place this queen in board[i][col]
+            board[i][col] = 1
+
+            # Recur to place the rest of the queens
+            if solveNQUtil(board, col + 1):
+                return True
+
+            # If placing queen in board[i][col] doesn't lead to a solution,
+            # then backtrack
+            board[i][col] = 0
+
+    # If the queen cannot be placed in any row in this column col then return False
+    return False
+
+def solveNQ():
+    board = [
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]
+    ]
+
+    if not solveNQUtil(board, 0):
+        print("Solution does not exist")
+        return False
+
+    printSolution(board)
+    return True
+
+# Driver Code
+if __name__ == '__main__':
+    solveNQ()
